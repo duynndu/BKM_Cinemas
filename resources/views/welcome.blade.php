1c61337f7f568021a5f22004fbd4f07ca3239974
duynndu <duynnz2812@gmail.com>
2024-08-20 21:42:17 +0700

    Thêm plugin tailwind daisyui flowbite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,4 @@
-@php use Illuminate\Support\Facades\Storage; @endphp
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -10,7 +9,18 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="antialiased">
+<body>
+<div class="max-w-7xl mx-auto">
+    <button class="btn btn-sm btn-primary">primary</button>
+    <details class="dropdown">
+        <summary class="btn btn-primary m-1">open or close</summary>
+        <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+        </ul>
+    </details>
+</div>
 </body>
 </html>
