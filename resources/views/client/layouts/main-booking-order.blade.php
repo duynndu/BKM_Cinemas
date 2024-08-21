7aade17eb15f6a86780d9654968146da51d02bfd
vanhcode <tranvanh2k4@gmail.com>
2024-08-21 23:04:09 +0700

    commit

---
+++ resources/views/client/layouts/main-booking-order.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<!--
+Template Name: Movie Pro
+Version: 1.0.0
+Author: Webstrot
+
+-->
+<!--[if IE 8]>
+<html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]>
+<html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="zxx">
+<!--[endif]-->
+
+
+<!-- Mirrored from www.webstrot.com/html/moviepro/html/booking_type.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 20 Aug 2024 15:06:33 GMT -->
+<head>
+    <meta charset="utf-8"/>
+    <title>@yield('title')</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <meta name="description" content="Movie Pro"/>
+    <meta name="keywords" content="Movie Pro"/>
+    <meta name="author" content=""/>
+    <meta name="MobileOptimized" content="320"/>
+    <!--Template style -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/animate.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/bootstrap.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/font-awesome.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/fonts.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/flaticon.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/owl.carousel.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/owl.theme.default.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/dl-menu.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/nice-select.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/magnific-popup.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/venobox.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/js/plugin/rs_slider/layers.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/js/plugin/rs_slider/navigation.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/js/plugin/rs_slider/settings.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/seat.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/style.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('movie/css/responsive.css') }}"/>
+    <link rel="stylesheet" id="theme-color" type="text/css" href="#"/>
+    <!-- favicon links -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('movie/images/header/favicon.ico') }}"/>
+</head>
+
+<body class="booking_type_back">
+<!-- preloader Start -->
+<div id="preloader">
+    <div id="status">
+        <img src="{{ asset('movie/images/header/horoscope.gif') }}" id="preloader_image" alt="loader">
+    </div>
+</div>
+@yield('content')
+<!--main js file start-->
+<script src="{{ asset('movie/js/jquery_min.js') }}"></script>
+<script src="{{ asset('movie/js/modernizr.js') }}"></script>
+<script src="{{ asset('movie/js/bootstrap.js') }}"></script>
+<script src="{{ asset('movie/js/owl.carousel.js') }}"></script>
+<script src="{{ asset('movie/js/jquery.dlmenu.js') }}"></script>
+<script src="{{ asset('movie/js/jquery.sticky.js') }}"></script>
+<script src="{{ asset('movie/js/jquery.nice-select.min.js') }}"></script>
+<script src="{{ asset('movie/js/jquery.magnific-popup.js') }}"></script>
+<script src="{{ asset('movie/js/jquery.bxslider.min.js') }}"></script>
+<script src="{{ asset('movie/js/venobox.min.js') }}"></script>
+<script src="{{ asset('movie/js/smothscroll_part1.js') }}"></script>
+<script src="{{ asset('movie/js/smothscroll_part2.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/jquery.themepunch.revolution.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/jquery.themepunch.tools.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.addon.snow.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.actions.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.carousel.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.kenburn.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.layeranimation.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.migration.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.navigation.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.parallax.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.slideanims.min.js') }}"></script>
+<script src="{{ asset('movie/js/plugin/rs_slider/revolution.extension.video.min.js') }}"></script>
+<script src="{{ asset('movie/js/custom.js') }}"></script>
+<!--main js file end-->
+<script>
+    //* Isotope js
+    function protfolioIsotope() {
+        if ($('.st_fb_filter_left_box_wrapper').length) {
+            // Activate isotope in container
+            $(".protfoli_inner, .portfoli_inner").imagesLoaded(function () {
+                $(".protfoli_inner, .portfoli_inner").isotope({
+                    layoutMode: 'masonry',
+                });
+            });
+
+            // Add isotope click function
+            $(".protfoli_filter li").on('click', function () {
+                $(".protfoli_filter li").removeClass("active");
+                $(this).addClass("active");
+                var selector = $(this).attr("data-filter");
+                $(".protfoli_inner, .portfoli_inner").isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 450,
+                        easing: "linear",
+                        queue: false,
+                    }
+                });
+                return false;
+            });
+        }
+        ;
+    };
+    protfolioIsotope();
+
+    function changeQty(increase) {
+        var qty = parseInt($('.select_number').find("input").val());
+        if (!isNaN(qty)) {
+            qty = increase ? qty + 1 : (qty > 1 ? qty - 1 : 1);
+            $('.select_number').find("input").val(qty);
+        } else {
+            $('.select_number').find("input").val(1);
+        }
+    }
+</script>
+</body>
+
+</html>
---
+++ resources/views/client/pages/event.blade.php
@@ -0,0 +1,279 @@
+@extends('client.layouts.main')
+
+@section('title', 'Sự kiện event')
+
+@section('content')
+    <div class="prs_title_main_sec_wrapper">
+        <div class="prs_title_img_overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="prs_title_heading_wrapper">
+                        <h2>Event-Categories</h2>
+                        <ul>
+                            <li><a href="#">Home</a>
+                            </li>
+                            <li>&nbsp;&nbsp; >&nbsp;&nbsp; Event</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- prs title wrapper End -->
+    <!-- prs ec ue wrapper Start -->
+    <div class="prs_ec_ue_main_section_wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="prs_heading_section_wrapper">
+                        <h2>Upcoming Events</h2>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="prs_ec_ue_left_img_wrapper">
+                        <img src="images/content/event/e1.jpg" alt="event_img">
+                    </div>
+                    <div class="prs_ec_ue_right_img_wrapper">
+                        <div class="prs_feature_img_cont prs_ec_ue_right_img_cont">
+                            <h2><a href="#">Music Event in india</a></h2>
+                            <div class="prs_ft_small_cont_left">
+                                <p>Mumbai & Pune</p>
+                            </div>
+                            <div class="prs_ft_small_cont_right"><i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <ul>
+                                <li>June 07 - july 08</li>
+                                <li>08:00-12:00 pm</li>
+                            </ul>
+                            <p class="prs_up_pera_sec">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiud
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo is aute irure dolor in reprehenderit in
+                                voluptate velit esse cillum fugiat nulla pariatur Excepteur sint. <a href="#">Read More</a>
+                            </p>
+                        </div>
+                        <div class="prs_ec_ue_timer_wrapper">
+                            <div id="clockdiv">
+                                <div><span class="days"></span>
+                                    <div class="smalltext">Days</div>
+                                </div>
+                                <div><span class="hours"></span>
+                                    <div class="smalltext">Hours</div>
+                                </div>
+                                <div><span class="minutes"></span>
+                                    <div class="smalltext">Mins</div>
+                                </div>
+                                <div><span class="seconds"></span>
+                                    <div class="smalltext">Secs</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="prs_feature_img_box_wrapper prs_ec_ue_inner_feat_wrapper">
+                        <div class="prs_feature_img">
+                            <img src="images/content/ft1.jpg" alt="feature_img">
+                            <div class="prs_ft_btn_wrapper">
+                                <ul>
+                                    <li><a href="#">Book Now</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="prs_feature_img_cont">
+                            <h2><a href="#">Music Event in india</a></h2>
+                            <div class="prs_ft_small_cont_left">
+                                <p>Mumbai & Pune</p>
+                            </div>
+                            <div class="prs_ft_small_cont_right"><i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <ul>
+                                <li>June 07 - july 08</li>
+                                <li>08:00-12:00 pm</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="prs_feature_img_box_wrapper prs_ec_ue_inner_feat_wrapper">
+                        <div class="prs_feature_img">
+                            <img src="images/content/ft2.jpg" alt="feature_img">
+                            <div class="prs_ft_btn_wrapper">
+                                <ul>
+                                    <li><a href="#">Book Now</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="prs_feature_img_cont">
+                            <h2><a href="#">Music Event in india</a></h2>
+                            <div class="prs_ft_small_cont_left">
+                                <p>Mumbai & Pune</p>
+                            </div>
+                            <div class="prs_ft_small_cont_right"><i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <ul>
+                                <li>June 07 - july 08</li>
+                                <li>08:00-12:00 pm</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="prs_feature_img_box_wrapper prs_ec_ue_inner_feat_wrapper">
+                        <div class="prs_feature_img">
+                            <img src="images/content/ft3.jpg" alt="feature_img">
+                            <div class="prs_ft_btn_wrapper">
+                                <ul>
+                                    <li><a href="#">Book Now</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="prs_feature_img_cont">
+                            <h2><a href="#">Music Event in india</a></h2>
+                            <div class="prs_ft_small_cont_left">
+                                <p>Mumbai & Pune</p>
+                            </div>
+                            <div class="prs_ft_small_cont_right"><i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <ul>
+                                <li>June 07 - july 08</li>
+                                <li>08:00-12:00 pm</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- prs ec ue wrapper End -->
+    <!-- prs feature slider Start -->
+    <div class="prs_ec_pe_slider_main_wrapper">
+        <div class="prs_ec_pe_slider_img_overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="prs_ec_pe_heading_section_wrapper">
+                        <h2>FEATURED EVENTS</h2>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="prs_feature_slider_wrapper">
+                        <div class="owl-carousel owl-theme">
+                            <div class="item">
+                                <div class="prs_feature_img_box_wrapper">
+                                    <div class="prs_feature_img">
+                                        <img src="images/content/event/e2.jpg" alt="feature_img">
+                                        <div class="prs_ft_btn_wrapper">
+                                            <ul>
+                                                <li><a href="#">Book Now</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="prs_feature_img_cont">
+                                        <h2><a href="#">Music Event in india</a></h2>
+                                        <div class="prs_ft_small_cont_left">
+                                            <p>Mumbai & Pune</p>
+                                        </div>
+                                        <div class="prs_ft_small_cont_right"><i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o"></i>
+                                            <i class="fa fa-star-o"></i>
+                                        </div>
+                                        <ul>
+                                            <li>June 07 - july 08</li>
+                                            <li>08:00-12:00 pm</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="prs_feature_img_box_wrapper">
+                                    <div class="prs_feature_img">
+                                        <img src="images/content/event/e3.jpg" alt="feature_img">
+                                        <div class="prs_ft_btn_wrapper">
+                                            <ul>
+                                                <li><a href="#">Book Now</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="prs_feature_img_cont">
+                                        <h2><a href="#">Music Event in india</a></h2>
+                                        <div class="prs_ft_small_cont_left">
+                                            <p>Mumbai & Pune</p>
+                                        </div>
+                                        <div class="prs_ft_small_cont_right"><i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o"></i>
+                                            <i class="fa fa-star-o"></i>
+                                        </div>
+                                        <ul>
+                                            <li>June 07 - july 08</li>
+                                            <li>08:00-12:00 pm</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="prs_feature_img_box_wrapper">
+                                    <div class="prs_feature_img">
+                                        <img src="images/content/event/e4.jpg" alt="feature_img">
+                                        <div class="prs_ft_btn_wrapper">
+                                            <ul>
+                                                <li><a href="#">Book Now</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="prs_feature_img_cont">
+                                        <h2><a href="#">Music Event in india</a></h2>
+                                        <div class="prs_ft_small_cont_left">
+                                            <p>Mumbai & Pune</p>
+                                        </div>
+                                        <div class="prs_ft_small_cont_right"><i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o"></i>
+                                            <i class="fa fa-star-o"></i>
+                                        </div>
+                                        <ul>
+                                            <li>June 07 - july 08</li>
+                                            <li>08:00-12:00 pm</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- prs feature slider End -->
+@endsection
+
+@section('js')
+
+@endsection
