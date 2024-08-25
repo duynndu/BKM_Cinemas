d08b2fff31511627090dbbdbfa63b347725791bd
duynndu <duynnz2812@gmail.com>
2024-08-25 15:36:38 +0700

    sửa lỗi migrate movie

--- database/migrations/2024_08_23_231005_create_movies_table.php
+++ database/migrations/2024_08_23_231005_create_movies_table.php
@@ -12,26 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('movies', function (Blueprint $table) {
-            Schema::create('movies', function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('title')->nullable();
-                $table->string('avatar_path')->nullable();
-                $table->string('description')->nullable();
-                $table->text('content')->nullable();
-                $table->integer('duration')->nullable();
-                $table->string('director')->nullable();
-                $table->json('cast')->nullable();
-                $table->string('trailer_url')->nullable();
-                $table->string('language')->nullable();
-                $table->date('release_date')->nullable();
-                $table->string('country')->nullable();
-                $table->string('genre')->nullable();
-                $table->integer('order')->nullable();
-                $table->tinyInteger('hot')->default(0);
-                $table->tinyInteger('active')->default(1);
-                $table->timestamps();
-                $table->softDeletes();
-            });
+            $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->string('avatar_path')->nullable();
+            $table->string('description')->nullable();
+            $table->text('content')->nullable();
+            $table->integer('duration')->nullable();
+            $table->string('director')->nullable();
+            $table->json('cast')->nullable();
+            $table->string('trailer_url')->nullable();
+            $table->string('language')->nullable();
+            $table->date('release_date')->nullable();
+            $table->string('country')->nullable();
+            $table->string('genre')->nullable();
+            $table->integer('order')->nullable();
+            $table->tinyInteger('hot')->default(0);
+            $table->tinyInteger('active')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
