0d9005a3c3bfd2f6f87a4ac159892816e07bfab3
vanhcode <tranvanh2k4@gmail.com>
2024-09-15 10:30:50 +0700

    update full source code

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,30 +12,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-//        // Tạo bảng vai trò (roles)
-//        Schema::create('roles', function (Blueprint $table) {
-//            $table->id();
-//            $table->string('name');
-//            $table->string('description')->nullable();
-//            $table->timestamps();
-//        });
-//
-//        // Tạo bảng chức năng (permissions)
-//        Schema::create('permissions', function (Blueprint $table) {
-//            $table->id();
-//            $table->string('name');
-//            $table->string('description')->nullable();
-//            $table->timestamps();
-//        });
-//
-//        // Tạo bảng phân quyền (role_permissions)
-//        Schema::create('permission_role', function (Blueprint $table) {
-//            $table->id();
-//            $table->foreignId('role_id')->constrained()->onDelete('cascade');
-//            $table->foreignId('permission_id')->constrained()->onDelete('cascade');
-//            $table->timestamps();
-//        });
-
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -43,7 +19,6 @@ return new class extends Migration
             $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-//            $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -54,12 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-//        DB::statement('DROP FOREIGN KEY refresh_tokens_user_id_foreign ON refresh_tokens');
-        Schema::dropIfExists('roles');
-        Schema::dropIfExists('functions');
-        Schema::dropIfExists('role_functions');
         Schema::dropIfExists('users');
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Clients\HomeController;
+use App\Http\Controllers\Client\HomeController;
 
 /*
 |--------------------------------------------------------------------------
