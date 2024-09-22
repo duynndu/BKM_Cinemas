90208b5ac327ec5429d463065fa8c4b7e9e36fca
vanhcode <tranvanh2k4@gmail.com>
2024-09-22 16:30:04 +0700

    push code CRUD Hệ Thống

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,6 @@
 namespace App\Providers;
 
 use App\Http\Views\Composers\GetAllDataComposer;
-use App\Repositories\Admin\CategoryPosts\Interface\CategoryPostInterface;
-use App\Repositories\Admin\CategoryPosts\Repository\CategoryPostRepository;
-use App\Repositories\Admin\Posts\Interface\PostInterface;
-use App\Repositories\Admin\Posts\Repository\PostRepository;
 use App\Repositories\Admin\Systems\Interface\SystemInterface;
 use App\Repositories\Admin\Systems\Repository\SystemRepository;
 use Illuminate\Support\ServiceProvider;
@@ -20,8 +16,6 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(SystemInterface::class, SystemRepository::class);
-        $this->app->bind(CategoryPostInterface::class, CategoryPostRepository::class);
-        $this->app->bind(PostInterface::class, PostRepository::class);
     }
 
     /**
