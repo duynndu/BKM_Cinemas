793231467b142be93f38ed57487a4ce3942a58d0
vanhcode <tranvanh2k4@gmail.com>
2024-09-22 13:59:29 +0700

    p

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,22 +8,5 @@ use App\Services\Admin\Dashboards\DashboardService;
 
 class DashboardController extends Controller
 {
-    protected $dashboardService;
-
-    public function __construct(
-        DashboardService $dashboardService
-    )
-    {
-        $this->dashboardService = $dashboardService;
-    }
-
-    public function dashboard()
-    {
-        $data['posts'] = $this->dashboardService->posts();
-
-        $data['top10PostLatest'] = $this->dashboardService->top10PostLatest();
-
-        return view('admin.pages.dashboard', compact('data'));
-    }
 
 }
