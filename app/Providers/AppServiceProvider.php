1324ddf52ed9721d73707efaa6fd102107d3fc75
Trần Nhật <you@example.com>
2024-11-05 23:41:50 +0700

    mobile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,31 +2,32 @@
 
 namespace App\Providers;
 
-use App\Repositories\Admin\Areas\Interface\AreaInterface;
-use App\Repositories\Admin\Areas\Repository\AreaRepository;
-use App\Repositories\Admin\CategoryPosts\Interface\CategoryPostInterface;
-use App\Repositories\Admin\CategoryPosts\Repository\CategoryPostRepository;
-use App\Repositories\Admin\Cities\Interface\CityInterface;
-use App\Repositories\Admin\Cities\Repository\CityRepository;
-use App\Repositories\Admin\Payments\Interface\PaymentInterface;
-use App\Repositories\Admin\Payments\Repository\PaymentRepository;
-use App\Repositories\Admin\Posts\Interface\PostInterface;
-use App\Repositories\Admin\Posts\Repository\PostRepository;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
 use App\Http\Views\Composers\GetAllDataComposer;
-use App\Repositories\Admin\Actors\Interface\ActorInterface;
-use App\Repositories\Admin\Foods\Interface\FoodComboInterface;
+use App\Repositories\Admin\Areas\Interface\AreaInterface;
 use App\Repositories\Admin\Foods\Interface\FoodInterface;
-use App\Repositories\Admin\Foods\Interface\FoodTypeInterface;
+use App\Repositories\Admin\Posts\Interface\PostInterface;
+use App\Repositories\Admin\Cities\Interface\CityInterface;
+use App\Repositories\Admin\Actors\Interface\ActorInterface;
+use App\Repositories\Admin\Areas\Repository\AreaRepository;
+use App\Repositories\Admin\Foods\Repository\FoodRepository;
+use App\Repositories\Admin\Posts\Repository\PostRepository;
+use App\Repositories\Admin\Cities\Repository\CityRepository;
 use App\Repositories\Admin\Actors\Repository\ActorRepository;
 use App\Repositories\Admin\Cinemas\Interface\CinemaInterface;
-use App\Repositories\Admin\Cinemas\Repository\CinemaRepository;
-use App\Repositories\Admin\Foods\Repository\FoodComboRepository;
-use App\Repositories\Admin\Foods\Repository\FoodRepository;
-use App\Repositories\Admin\Foods\Repository\FoodTypeRepository;
+use App\Repositories\Admin\Foods\Interface\FoodTypeInterface;
 use App\Repositories\Admin\Systems\Interface\SystemInterface;
+use App\Repositories\Admin\Foods\Interface\FoodComboInterface;
+use App\Repositories\Admin\Cinemas\Repository\CinemaRepository;
+use App\Repositories\Admin\Foods\Repository\FoodTypeRepository;
+use App\Repositories\Admin\Payments\Interface\PaymentInterface;
 use App\Repositories\Admin\Systems\Repository\SystemRepository;
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
+use App\Repositories\Admin\Foods\Repository\FoodComboRepository;
+use App\Repositories\Admin\Payments\Repository\PaymentRepository;
+use App\Repositories\Admin\CategoryPosts\Interface\CategoryPostInterface;
+use App\Repositories\Admin\CategoryPosts\Repository\CategoryPostRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -36,16 +37,16 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(CategoryPostInterface::class, CategoryPostRepository::class);
-        $this->app->bind(PostInterface        ::class, PostRepository        ::class);
-        $this->app->bind(CityInterface        ::class, CityRepository        ::class);
-        $this->app->bind(AreaInterface        ::class, AreaRepository        ::class);
-        $this->app->bind(PaymentInterface     ::class, PaymentRepository     ::class);
-        $this->app->bind(SystemInterface      ::class, SystemRepository      ::class);
-        $this->app->bind(FoodTypeInterface    ::class, FoodTypeRepository    ::class);
-        $this->app->bind(FoodInterface        ::class, FoodRepository        ::class);
-        $this->app->bind(FoodComboInterface   ::class, FoodComboRepository   ::class);
-        $this->app->bind(ActorInterface       ::class, ActorRepository       ::class);
-        $this->app->bind(CinemaInterface      ::class, CinemaRepository      ::class);
+        $this->app->bind(PostInterface::class, PostRepository::class);
+        $this->app->bind(CityInterface::class, CityRepository::class);
+        $this->app->bind(AreaInterface::class, AreaRepository::class);
+        $this->app->bind(PaymentInterface::class, PaymentRepository::class);
+        $this->app->bind(SystemInterface::class, SystemRepository::class);
+        $this->app->bind(FoodTypeInterface::class, FoodTypeRepository::class);
+        $this->app->bind(FoodInterface::class, FoodRepository::class);
+        $this->app->bind(FoodComboInterface::class, FoodComboRepository::class);
+        $this->app->bind(ActorInterface::class, ActorRepository::class);
+        $this->app->bind(CinemaInterface::class, CinemaRepository::class);
     }
 
     /**
@@ -55,5 +56,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // Admin
         View::composer('admin.partials.sidebar', GetAllDataComposer::class);
+
+        // if (env('APP_ENV') !== 'local') {
+        //     URL::forceScheme('https');
+        // }
     }
 }
